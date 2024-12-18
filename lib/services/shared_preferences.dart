c40531aac7b68cd0059630a843880953d8fb9449
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-18 19:19:24 +0200

    create addToddo method

--- lib/services/shared_preferences.dart
+++ lib/services/shared_preferences.dart
@@ -3,4 +3,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesService{
 final SharedPreferences sharedPreferences;
 SharedPreferencesService ( this.sharedPreferences ) ;
+
+ Future<void> addTodo(String value) async {
+final result = sharedPreferences.getStringList( ' items' ) ;
+result?. add(value);
+await sharedPreferences. setStringList(  'items', result ?? [] ) ;
+
+ }
+
+
 }
