29083a3abfc029ae1512e6f10c9bdebdaf743bea
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-18 21:03:42 +0200

    ensure that the note is created and stored

--- lib/services/shared_preferences.dart
+++ lib/services/shared_preferences.dart
@@ -1,15 +1,49 @@
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// class SharedPreferencesService{
+// final SharedPreferences sharedPreferences;
+// SharedPreferencesService ( this.sharedPreferences ) ;
+
+//  Future<void> addTodo(String value) async {
+// final result = sharedPreferences.getStringList('Items') ;
+// result?.add(value);
+// await sharedPreferences.setStringList('Items', result ?? [] ) ;
+// print(" set todo");
+
+//  }
+
+
+// Future<List<String>> getTodo() async {
+//   print(" get todo");
+//   return sharedPreferences.getStringList('Items') ?? [] ;
+//  }
+
+
+
+// }
+
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPreferencesService{
-final SharedPreferences sharedPreferences;
-SharedPreferencesService ( this.sharedPreferences ) ;
+class SharedPreferencesService {
+  final SharedPreferences sharedPreferences;
 
- Future<void> addTodo(String value) async {
-final result = sharedPreferences.getStringList( ' items' ) ;
-result?. add(value);
-await sharedPreferences. setStringList(  'items', result ?? [] ) ;
+  SharedPreferencesService(this.sharedPreferences);
 
- }
+  Future<void> addTodo(String value) async {
+    // Get the current list of items
+    final result = sharedPreferences.getStringList('Items') ?? [];
+    
+    // Add the new value to the list
+    result.add(value);
+    
+    // Save the updated list back to SharedPreferences
+    await sharedPreferences.setStringList('Items', result);
+    
+    print("Added todo: $value");
+  }
 
-
+  Future<List<String>> getTodo() async {
+    print("Getting todos");
+    return sharedPreferences.getStringList('Items') ?? [];
+  }
 }
